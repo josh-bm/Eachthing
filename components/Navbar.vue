63845c47d3ff6f9f4d02d3189039ce7319cb9aae
josh-bm <32262804+josh-bm@users.noreply.github.com>
2022-06-24 07:27:33 +0200

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -317,7 +317,7 @@ const up = ref(false);
           </div>
         </div>
 
-        <div class="flex items-center lg:hidden">
+        <div class="flex justify-between items-center lg:hidden">
           <!-- Mobile menu button -->
           <DisclosureButton
             class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
@@ -330,9 +330,14 @@ const up = ref(false);
         </div>
       </div>
 
-      <DisclosurePanel class="lg:hidden shadow-lg rounded-lg">
+      <DisclosurePanel class="lg:hidden w-full">
         <div class="pt-2 pb-3 space-y-1">
-          <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" -->
+          <nuxt-link
+            to="/"
+            class="block pl-3 pr-4 py-2 hover:bg-teal-800 hover:text-white text-base font-medium"
+          >
+            Home
+          </nuxt-link>
           <DisclosureButton
             as="a"
             v-for="item in navigation"
@@ -346,7 +351,7 @@ const up = ref(false);
           <nuxt-link
             to=""
             @click="up = true"
-            class="cursor-pointer text-lg font-medium hover:text-teal-800"
+            class="cursor-pointer font-medium hover:text-teal-800"
             >Log in</nuxt-link
           >
 
